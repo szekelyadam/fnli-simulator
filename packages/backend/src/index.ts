461e54daf4f6b593d7e761dd35231f5c749f2004
Ádám Székely <adam@szekely.xyz>
2025-05-05 20:11:50 +0200

    Store winning tickets data to DB

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,22 +1,46 @@
 import WebSocket from "ws";
 import { countMatchingNumbers, drawNumbers } from "./utils";
+import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
+import { LOWEST_MATCH_COUNT } from "./consts";
 
 const wss = new WebSocket.Server({ port: 8080 });
+
+const prisma = new PrismaClient();
 
 wss.on("error", (error) => {
     console.error(error);
 });
 
-wss.on("connection", (ws: WebSocket) => {
+wss.on("connection", async (ws: WebSocket) => {
     console.log("Client connected");
 
-    ws.on("message", (message: string) => {
+    const sessionId = uuidv4();
+
+    ws.on("message", async (message: string) => {
         const data = JSON.parse(message) as { type: string; numbers: number[] };
 
         if (data.type === "draw") {
             const drawnNumbers = drawNumbers();
 
             const matches = countMatchingNumbers(data.numbers, drawnNumbers);
+
+            if (matches >= LOWEST_MATCH_COUNT) {
+                const winningTicket = {
+                    playedNumbers: data.numbers,
+                    drawnNumbers,
+                    matchCount: matches,
+                    sessionId: sessionId,
+                };
+
+                try {
+                    await prisma.winningTicket.create({
+                        data: winningTicket,
+                    });
+                } catch (error) {
+                    console.error("DB error: ", error);
+                }
+            }
 
             ws.send(
                 JSON.stringify({
