aa66c1317ad1ae1bdc698605389a85b037aa0276
Ádám Székely <adam@szekely.xyz>
2025-05-07 07:45:31 +0200

    Styles OverallStatsContainer

--- packages/frontend/src/components/OverallStatsContainer.tsx
+++ packages/frontend/src/components/OverallStatsContainer.tsx
@@ -8,10 +8,15 @@ const OverallStatsContainer = ({
     playedTickets,
 }: OverallStatsContainerProps) => {
     return (
-        <div>
-            Number of tickets: {playedTickets}
-            Years spent: {Math.floor(playedTickets / 52)}
-            Cost of tickets: {playedTickets * TICKET_PRICE} Ft
+        <div className="grid grid-cols-2 gap-x-8 gap-y-2 w-full text-white bg-primary rounded-lg px-6 py-4 font-bold">
+            <span>Number of tickets</span>
+            <span className="font-bold">{playedTickets}</span>
+
+            <span>Years spent</span>
+            <span className="font-bold">{Math.floor(playedTickets / 52)}</span>
+
+            <span>Cost of tickets</span>
+            <span className="font-bold">{playedTickets * TICKET_PRICE} Ft</span>
         </div>
     );
 };
